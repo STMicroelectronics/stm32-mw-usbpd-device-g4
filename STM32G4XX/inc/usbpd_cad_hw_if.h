1a28844410128ad7d06e34313f086bf58d1423f1
karim <karim.rabhi-ext@st.com>
2025-12-26 16:22:28 +0100

    Release v3.0.0

--- STM32G4XX/inc/usbpd_cad_hw_if.h
+++ STM32G4XX/inc/usbpd_cad_hw_if.h
@@ -1,15 +1,15 @@
 /**
   ******************************************************************************
-  * @file    usbpd_timersserver.h
+  * @file    usbpd_cad_hw_if.h
   * @author  MCD Application Team
-  * @brief   This file contains the headers of usbpd_timerserver.h.
+  * @brief   This file contains the headers of usbpd_cad.h for Cable Attach-Detach
+  *          controls.
   ******************************************************************************
   * @attention
   *
   * <h2><center>&copy; Copyright (c) 2018 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
-  * Redistribution and use in source and binary forms, with or without
   * This software component is licensed by ST under Ultimate Liberty license
   * SLA0044, the "License"; You may not use this file except in compliance with
   * the License. You may obtain a copy of the License at:
@@ -17,15 +17,15 @@
   *
   ******************************************************************************
   */
-
-#ifndef __USBPD_TIMERSSERVER_H_
-#define __USBPD_TIMERSSERVER_H_
+#ifndef __USBPD_CAD_HW_IF_H_
+#define __USBPD_CAD_HW_IF_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+
 /** @addtogroup STM32_USBPD_LIBRARY
   * @{
   */
@@ -34,29 +34,30 @@ extern "C" {
   * @{
   */
 
-/** @addtogroup USBPD_DEVICE_TIMESERVER
+/** @addtogroup USBPD_DEVICE_CAD_HW_IF
   * @{
   */
 
-/* Exported constants --------------------------------------------------------*/
-typedef enum {
-  TIM_PORT0_CRC,
-  TIM_PORT0_RETRY,
-  TIM_PORT1_CRC,
-  TIM_PORT1_RETRY,
-  TIM_MAX
-}
-TIM_identifier;
 
-#define TIM_MAX_TIME 10000u /*time in us, means 10 ms */
 /* Exported types ------------------------------------------------------------*/
-/* External variables --------------------------------------------------------*/
-/* Exported macros -----------------------------------------------------------*/
+/* Exported define -----------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported variables --------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
-void     USBPD_TIM_Init(void);
-void     USBPD_TIM_DeInit(void);
-void     USBPD_TIM_Start(TIM_identifier Id, uint32_t TimeUs);
-uint32_t USBPD_TIM_IsExpired(TIM_identifier Id);
+
+/** @addtogroup USBPD_DEVICE_CAD_HW_IF_Exported_Functions
+  * @{
+  */
+void CAD_Init(uint8_t PortNum, USBPD_SettingsTypeDef *Settings, USBPD_ParamsTypeDef *Params, void (*PtrWakeUp)(void));
+uint32_t CAD_StateMachine(uint8_t PortNum, USBPD_CAD_EVENT *Event, CCxPin_TypeDef *CCXX);
+void CAD_Enter_ErrorRecovery(uint8_t PortNum);
+uint32_t CAD_Set_ResistorRp(uint8_t PortNum, CAD_RP_Source_Current_Adv_Typedef RpValue);
+
+/**
+  * @}
+  */
 
 /**
   * @}
@@ -74,7 +75,6 @@ uint32_t USBPD_TIM_IsExpired(TIM_identifier Id);
 }
 #endif
 
-#endif /* __USBPD_TIMERSSERVER_H_ */
+#endif /* __USBPD_CAD_HW_IF_H_ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
